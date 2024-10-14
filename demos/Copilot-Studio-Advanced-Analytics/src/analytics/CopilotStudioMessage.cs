04212f21489003b8d72e6b32b118feed12dc15d9
Tim Hanewich <tahanewich@live.com>
2024-10-14 16:12:30 -0400

    Default empty string

--- demos/Copilot-Studio-Advanced-Analytics/src/analytics/CopilotStudioMessage.cs
+++ demos/Copilot-Studio-Advanced-Analytics/src/analytics/CopilotStudioMessage.cs
@@ -6,5 +6,11 @@ namespace CopilotStudioAnalytics
     {
         public string Role {get; set;} //i.e. "Copilot" or "User"
         public string Text {get; set;} //the message itself
+
+        public CopilotStudioMessage()
+        {
+            Role = "";
+            Text = "";
+        }
     }
 }
