dd646852e4632d93caf3df13d9921aa6c063cb65
Tim Hanewich <tahanewich@live.com>
2025-05-27 19:47:04 -0400

    Testing

--- demos/EVVIE/src/core/Program.cs
+++ demos/EVVIE/src/core/Program.cs
@@ -8,13 +8,10 @@ namespace VehicleInspectionAI
     {
         public static void Main(string[] args)
         {
-            string imgpath = @"C:\Users\timh\Downloads\odo3.jpg";
+            string imgpath = @"C:\Users\timh\Downloads\plate.jpg";
             byte[] imgbytes = File.ReadAllBytes(imgpath);
             string b64 = "data:image/jpeg;base64," + Convert.ToBase64String(imgbytes);
             System.IO.File.WriteAllText(@"C:\Users\timh\Downloads\SLG-Business-Applications\demos\EVVIE\src\core\B64.txt", b64);
-
-            int val = OdometerAgent.ReadOdometerAsync(b64).Result;
-            Console.WriteLine(val);
         }
     }
 }
