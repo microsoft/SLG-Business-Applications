7dccbb4f53796595d77bc5f247e830949d03ce35
Tim Hanewich <tahanewich@live.com>
2025-05-27 19:54:09 -0400

    Changed port to 6300

--- demos/EVVIE/src/api-aspnet/Program.cs
+++ demos/EVVIE/src/api-aspnet/Program.cs
@@ -8,7 +8,7 @@ namespace EVVIE_API
         {
             //Set up builder
             var builder = WebApplication.CreateBuilder(args);
-            builder.WebHost.UseUrls("http://0.0.0.0:5000");
+            builder.WebHost.UseUrls("http://0.0.0.0:6300");
             builder.Services.AddControllers();
             
             //Run the app
