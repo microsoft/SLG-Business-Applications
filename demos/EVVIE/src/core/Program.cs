721ec86efeac43d2959d0ea0a05c60c99b2e1f5d
Tim Hanewich <tahanewich@live.com>
2025-06-05 14:12:21 -0400

    Developed way to resize

--- demos/EVVIE/src/core/Program.cs
+++ demos/EVVIE/src/core/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace VehicleInspectionAI
 {
@@ -8,23 +10,52 @@ namespace VehicleInspectionAI
     {
         public static void Main(string[] args)
         {
-            string folder = @"C:\Users\timh\Downloads\CURTIS";
+            ResizeAndB64ConvertBulk(@"C:\Users\timh\Downloads\CURTIS", 0.25f);
+        }
+
+        public static void ResizeAndB64ConvertBulk(string folder, float resize = 1.0f)
+        {
             string[] files = System.IO.Directory.GetFiles(folder);
+
             foreach (string file in files)
             {
                 string name = Path.GetFileName(file);
+                string nameNoExtension = Path.GetFileNameWithoutExtension(file);
 
                 //Convert to base64
-                byte[] content = System.IO.File.ReadAllBytes(file);
-                string b64 = Convert.ToBase64String(content);
-                b64 = "data:image/jpeg;base64," + b64;
+                string b64 = ImageToBase64(file, resize);
 
-                //Plop in drop
-                System.IO.File.WriteAllText(@"C:\Users\timh\Downloads\SLG-Business-Applications\demos\EVVIE\src\core\DROP.txt", b64);
+                //Save
+                string newname = nameNoExtension + ".txt";
+                string newpath = Path.Combine(folder, newname);
+                System.IO.File.WriteAllText(newpath, b64);
 
-                Console.Write("File '" + name + "' base64 now dropped. Enter to continue...");
-                Console.ReadLine();
+                //Print status!
+                Console.WriteLine("Converted '" + name + "' to '" + Path.GetFileName(newpath) + "'");
             }
+
+
         }
+
+        public static string ImageToBase64(string path, float resize = 1.0f)
+        {
+            //Perform resize
+            Image i = Image.FromFile(path);
+            int NewWidth = Convert.ToInt32(Convert.ToSingle(i.Width) * resize);
+            int NewHeight = Convert.ToInt32(Convert.ToSingle(i.Height) * resize);
+            Bitmap Resized = new Bitmap(NewWidth, NewHeight);
+            Graphics g = Graphics.FromImage(Resized);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.DrawImage(i, 0, 0, NewWidth, NewHeight);
+
+            //Save to memory stream
+            MemoryStream ms = new MemoryStream();
+            Resized.Save(ms, ImageFormat.Jpeg);
+            byte[] imgBytes = ms.ToArray();
+            string b64 = "data:image/jpeg;base64," + Convert.ToBase64String(imgBytes);
+
+            return b64;
+        }
+
     }
 }
