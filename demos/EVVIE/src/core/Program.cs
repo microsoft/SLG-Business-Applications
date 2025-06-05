92cf8f9bc50dc3aa58bca49797ca840276c190d2
Tim Hanewich <tahanewich@live.com>
2025-06-05 11:02:06 -0400

    Modified code to drop one by one

--- demos/EVVIE/src/core/Program.cs
+++ demos/EVVIE/src/core/Program.cs
@@ -8,10 +8,23 @@ namespace VehicleInspectionAI
     {
         public static void Main(string[] args)
         {
-            byte[] content = System.IO.File.ReadAllBytes(@"C:\Users\timh\Downloads\CRACKED.jpg");
-            string b64 = Convert.ToBase64String(content);
-            b64 = "data:image/jpeg;base64," + b64;
-            System.IO.File.WriteAllText(@"C:\Users\timh\Downloads\SLG-Business-Applications\demos\EVVIE\b64.txt", b64);
+            string folder = @"C:\Users\timh\Downloads\CURTIS";
+            string[] files = System.IO.Directory.GetFiles(folder);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+
+                //Convert to base64
+                byte[] content = System.IO.File.ReadAllBytes(file);
+                string b64 = Convert.ToBase64String(content);
+                b64 = "data:image/jpeg;base64," + b64;
+
+                //Plop in drop
+                System.IO.File.WriteAllText(@"C:\Users\timh\Downloads\SLG-Business-Applications\demos\EVVIE\src\core\DROP.txt", b64);
+
+                Console.Write("File '" + name + "' base64 now dropped. Enter to continue...");
+                Console.ReadLine();
+            }
         }
     }
 }
